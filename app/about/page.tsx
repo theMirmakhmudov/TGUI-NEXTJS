30e2b4ca2f7f476c2bff87fc862c462edb038535
mrmirmakhmudov <mr.mirmakhmudov16112008@gmail.com>
2024-08-26 10:00:43 +0500

    Test

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,39 +1,29 @@
-// 'use client';
+'use client';
 
-// import '@telegram-apps/telegram-ui/dist/styles.css';
-// import { AppRoot, List, Section, Cell } from '@telegram-apps/telegram-ui';
-// import { useRouter } from 'next/navigation';
-// import { UserIcon,UsersIcon, InfoIcon  } from 'lucide-react'; 
-// const cellsTexts = [
-//   { text: 'Profile', Icon: UserIcon },
-//   { text: 'Group', Icon: UsersIcon }, 
-//   { text: 'About', Icon: InfoIcon }, 
-// ];
+import '@telegram-apps/telegram-ui/dist/styles.css';
+import { AppRoot, List, Section, Cell } from '@telegram-apps/telegram-ui';
+import { useRouter } from 'next/navigation';
+import { UserIcon,UsersIcon, InfoIcon  } from 'lucide-react'; 
+const cellsTexts = [
+  { text: 'Profile', Icon: UserIcon },
+  { text: 'Group', Icon: UsersIcon }, 
+  { text: 'About', Icon: InfoIcon }, 
+];
 
-// const AboutPage = () => {
-//   const router = useRouter();
+const AboutPage = () => {
+  const router = useRouter();
 
-//   return (
-//     <AppRoot>
-//       <Section>
-//           {cellsTexts.map((cell, index) => (
-//             <Cell key={index} before={<cell.Icon size={24} />}>
-//               {cell.text}
-//             </Cell>
-//           ))}
-//         </Section>
-//     </AppRoot>
-//   );
-// };
-
-// export default AboutPage;
-
-import React from 'react'
-
-const page = () => {
   return (
-    <div>About Page</div>
-  )
-}
+    <AppRoot>
+      <Section>
+          {cellsTexts.map((cell, index) => (
+            <Cell key={index} before={<cell.Icon size={24} />}>
+              {cell.text}
+            </Cell>
+          ))}
+        </Section>
+    </AppRoot>
+  );
+};
 
-export default page
+export default AboutPage;
--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,46 +1,36 @@
-// 'use client';
+'use client';
 
-// import '@telegram-apps/telegram-ui/dist/styles.css';
-// import { AppRoot,Placeholder } from '@telegram-apps/telegram-ui';
-// import { useRouter } from 'next/navigation';
+import '@telegram-apps/telegram-ui/dist/styles.css';
+import { AppRoot,Placeholder } from '@telegram-apps/telegram-ui';
+import { useRouter } from 'next/navigation';
 
-// declare global {
-//   interface Window {
-//     Telegram: any;
-//   }
-// }
-
-
-// const TeamPage = () => {
-//   const router = useRouter();
-
-//   return (
-//     <AppRoot>
-//       <div className="HIJtihMA8FHczS02iWF5">
-//   <Placeholder
-//     description="Description"
-//     header="Title"
-//   >
-//     <img
-//       alt="Telegram sticker"
-//       className="blt0jZBzpxuR4oDhJc8s"
-//       src="https://xelene.me/telegram.gif"
-//     />
-//   </Placeholder>
-// </div>
-
-//     </AppRoot>
-//   );
-// };
-
-// export default TeamPage;
-
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>Team Page</div>
-  )
+declare global {
+  interface Window {
+    Telegram: any;
+  }
 }
 
-export default page
+
+const TeamPage = () => {
+  const router = useRouter();
+
+  return (
+    <AppRoot>
+      <div className="HIJtihMA8FHczS02iWF5">
+  <Placeholder
+    description="Description"
+    header="Title"
+  >
+    <img
+      alt="Telegram sticker"
+      className="blt0jZBzpxuR4oDhJc8s"
+      src="https://xelene.me/telegram.gif"
+    />
+  </Placeholder>
+</div>
+
+    </AppRoot>
+  );
+};
+
+export default TeamPage;
